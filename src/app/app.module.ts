99eeb1e38d8208b58dbb148b3767ae2b855f5727
IvanPKostadinov <ivanpkostadinov@gmail.com>
2021-10-30 13:48:14 +0300

    added movie-list with item and cast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MoviesComponent } from './movies/movies.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
+import { MovieItemComponent } from './components/movies/movie-list/movie-item/movie-item.component';
+import { MovieCastComponent } from './components/movies/movie-list/movie-cast/movie-cast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieListComponent,
+    MovieItemComponent,
+    MovieCastComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
